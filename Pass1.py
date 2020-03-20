304ea05b6f22db0b8fcb6fcafe84ef9bfd489386
Osama <maharmeh.osama98@gmail.com>
2020-03-20 21:45:30 +0200

    print the output to dile

--- Pass1.py
+++ Pass1.py
@@ -47,6 +47,7 @@ class Assembler:
         self.prog_name = ''
         self.start_address = 0
         self.prog_length = 0
+        self.intermediate = []
 
     def is_comment(self, line):
         return line.split()[0].startswith('.')
@@ -59,7 +60,6 @@ class Assembler:
         # Find the starting address and the name of the program.
         first_line = Line(next(self.content))
         label, operation_name, operand = first_line.label, first_line.operation_name, first_line.operand
-        
 
         if operation_name is not None:
             if operation_name == 'START':
@@ -69,11 +69,15 @@ class Assembler:
         # To jump on the first line
         first_line.line_location = self.start_address
 
+        self.intermediate.append(first_line)
+
         for line_number, line in enumerate(self.content):
             if not self.is_empty(line) and not self.is_comment(line):
                 line_object = Line(line)
                 label, operation_name, operand = line_object.label, line_object.operation_name, line_object.operand
                 line_object.line_location = self.locctr
+
+                self.intermediate.append(line_object)
 
                 if label is not None:
                     if label not in self.symbol_table:
@@ -116,7 +120,25 @@ class Assembler:
         self.prog_length = int(hex(self.locctr - self.start_address), 0)
 
 
-with open(r'C:\Users\aaxxo\Desktop\SICass\sample_tests\page58.asm') as file:
-    assembler=Assembler(file)
-    assembler.pass_one()
-    print(assembler.prog_name, assembler.prog_length,  assembler.symbol_table, sep='\n')
+def assembel(source_file_path):
+    with open(source_file_path, 'r') as source_file, open(r'C:\Users\aaxxo\Desktop\SICass\intermediate.txt', 'w') as intermediate_file:
+        assembler = Assembler(source_file)
+        assembler.pass_one()
+        # print(assembler.prog_name, assembler.prog_length,  assembler.symbol_table, sep='\n')
+        # return assembler.prog_name, assembler.prog_length,  assembler.symbol_table
+
+        # lines = [' '.join(line) for line in assembler.intermediate]
+        # intermediate_file.writelines(lines)
+
+        # print(['\n' + line.label if line.label is not None else '\n    ' + line.operation_name + line.operand for line in assembler.intermediate])
+
+        # for line_object in assembler.intermediate:
+        #     print([hex(line_object.line_location), line_object.label if line_object.label is not None else '    ',
+        #            line_object.operation_name, line_object.operand if line_object.operand is not None else '    '])
+        test = '\n'.join(['\t'.join([hex(line_object.line_location), line_object.label if line_object.label is not None else '\t',
+                                     line_object.operation_name, line_object.operand if line_object.operand is not None else '\t']) for line_object in assembler.intermediate])
+        intermediate_file.write(test)
+        print(test)
+
+
+assembel(r'sample_tests\basic.asm')
